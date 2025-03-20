be44e76415fd251d41aac97c2e7b5f0ba4ffa6f7
Henrik Augustsson <henrik.augustsson@nordicmedtest.se>
2025-03-20 15:00:43 +0100

    Upgrading jakarta.xml.soap-api package 1.4.2 -> 3.0.2, to match needed version in ApSE.

--- src/main/java/se/inera/pascal/ticket/core/SAML2AssertionTicketGenerator.java
+++ src/main/java/se/inera/pascal/ticket/core/SAML2AssertionTicketGenerator.java
@@ -2,7 +2,7 @@ package se.inera.pascal.ticket.core;
 
 import java.util.List;
 
-import javax.xml.soap.SOAPMessage;
+import jakarta.xml.soap.SOAPMessage;
 
 import org.opensaml.saml2.core.Assertion;
 import org.opensaml.ws.soap.client.SOAPFaultException;
